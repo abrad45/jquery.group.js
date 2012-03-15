0990260e556f3543a867450f3eb185ea729c1db4
Alexander Bradley <alexanderwbradley@gmail.com>
2012-03-14 20:58:28 -0400

    Fixed Issue #3; Removed GPL Licensing
    MIT licensing only now.

--- jquery.group.js
+++ jquery.group.js
@@ -2,9 +2,13 @@
  * jQuery Group Plugin
  * Examples and Documentation at 
  * https://github.com/abrad45/jquery.group.js
- * Copyright (c) 2011-2012 Alexander Bradley
  * Version 1.0.3 (9 March 2012 23:31 EST)
- * Dual licensed under the MIT and GPL licenses.
+ * Copyright (c) 2011-2012 Alexander Bradley
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
  * Requires jQuery 1.4
  */
 
@@ -22,7 +26,7 @@
 					options.classing = a; 
 					break; 
 				case Number: 
-					options.size = parseInt(Math.ceil(a)); 
+					options.size = Math.ceil(a); 
 					break; 
 				case String: 
 					if(a.charAt(0) === '.') {
